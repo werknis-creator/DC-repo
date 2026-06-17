68da7e5bd78c0dabb5601be3e8143a42eaacc902
werknis-creator <werknis@gmail.com>
2026-06-17 16:45:59 +0200

    Update main.c

--- main.c
+++ main.c
@@ -4,10 +4,10 @@ KOS_INIT_FLAGS(INIT_DEFAULT);
 
 int main(int argc, char **argv) {
     dbgio_dev_default();
-    printf("Działa na konsoli!\n");
+    printf("Dziala na konsoli Sega Dreamcast!\n");
     
     while(1) {
-        // Tu będziemy pisać główną pętlę gry
+        // Główna pętla gry (narazie pusta)
     }
     return 0;
 }
